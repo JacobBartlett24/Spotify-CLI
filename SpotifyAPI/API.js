533dd7a0ddf8d7dcba8165941d00b6a0034e6062
JacobBartlett24 <mbart6869@gmail.com>
2022-12-05 15:58:55 -0600

    Add code obtainer

--- SpotifyAPI/API.js
+++ SpotifyAPI/API.js
@@ -4,22 +4,23 @@ const { useState } = require('react');
 const clientID = "c5f6e12045d44f5bb46e0382bcff9060"
 const clientSecret = "28c70e35b7464a0d8965ee82034ee87b"
 const redirectURI = "http://localhost:5500/index.html"
+const bodyParser = require('body-parser');
 
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen('8080', ()=>{
+  console.log('node process')
+})
 
 const API = async () =>{
-  
-  let app = express(); 
-  var scope = 'user-read-private user-read-email';
+  let scope = 'user-read-private user-read-email';
 
-  app.get('/index.html'), (req,res) =>{
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-
-    console.log(code)
-  }
-  
-  open(`https://accounts.spotify.com/authorize?response_type=${'code'}&client_id=${clientID}&scope=${scope}&redirect_uri=${redirectURI}`)
-
+  open(`https://accounts.spotify.com/authorize?response_type=${'code'}&client_id=${clientID}&scope=${scope}&redirect_uri=${redirectURI}`);
 }
+
+app.post('/post', (req,res)=>{
+  res.send(`code = ${req.body.code}`);
+});
 
 module.exports = API
