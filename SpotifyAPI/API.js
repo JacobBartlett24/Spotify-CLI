8ae7b98b81f8535455c5d1280c259d2f610b325f
JacobBartlett24 <mbart6869@gmail.com>
2022-12-05 20:21:43 -0600

    Add axios dependency

--- SpotifyAPI/API.js
+++ SpotifyAPI/API.js
@@ -5,6 +5,7 @@ const clientID = "c5f6e12045d44f5bb46e0382bcff9060"
 const clientSecret = "28c70e35b7464a0d8965ee82034ee87b"
 const redirectURI = "http://localhost:5500/index.html"
 const bodyParser = require('body-parser');
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,14 +14,18 @@ app.listen('8080', ()=>{
   console.log('node process')
 })
 
-const API = async () =>{
+const API = () =>{
+  const [code,setCode] = useState('')
+
   let scope = 'user-read-private user-read-email';
 
   open(`https://accounts.spotify.com/authorize?response_type=${'code'}&client_id=${clientID}&scope=${scope}&redirect_uri=${redirectURI}`);
+
+  app.post('/post', (req,res)=>{
+    res.send(`code = ${req.body.code}`);
+  });
 }
 
-app.post('/post', (req,res)=>{
-  res.send(`code = ${req.body.code}`);
-});
+
 
 module.exports = API
