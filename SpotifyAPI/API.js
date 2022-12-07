7414d51c715e72f548f7747adc6c6134f545764b
JacobBartlett24 <mbart6869@gmail.com>
2022-12-06 19:09:38 -0600

    Add correct API call

--- SpotifyAPI/API.js
+++ SpotifyAPI/API.js
@@ -6,6 +6,7 @@ const clientSecret = "28c70e35b7464a0d8965ee82034ee87b"
 const redirectURI = "http://localhost:5500/index.html"
 const bodyParser = require('body-parser');
 const axios = require('axios')
+const qs = require('qs');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,14 +16,32 @@ app.listen('8080', ()=>{
 })
 
 const API = () =>{
-  const [code,setCode] = useState('')
-
   let scope = 'user-read-private user-read-email';
 
   open(`https://accounts.spotify.com/authorize?response_type=${'code'}&client_id=${clientID}&scope=${scope}&redirect_uri=${redirectURI}`);
 
-  app.post('/post', (req,res)=>{
-    res.send(`code = ${req.body.code}`);
+  const token_url = 'https://accounts.spotify.com/api/token';
+
+  app.get('/post', async (req,res)=>{
+    try{
+      const data = {
+        "grant_type": "authorization_code",
+        "code": req.query.code,
+        "redirect_uri": redirectURI,
+        "client_id": clientID,
+        "client_secret": clientSecret,
+      }
+      const response = await axios.post(token_url, data, {
+        headers: { 
+          'Authorization': 'Basic ' + (new Buffer.from(clientID + ':' + clientSecret).toString('base64')),
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      console.log(response)
+      }catch(err){
+        console.log(err)
+      }
+
   });
 }
 
